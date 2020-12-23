6351275632b060b4d14f5c335019cacb0b2d965c
Fong Yoong <fongyoong8@users.noreply.github.com>
2020-12-23 16:32:00 +0800

    Added popup

--- popup.js
+++ popup.js
@@ -6,7 +6,7 @@ button.onclick = function(element) {
     chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
 
         if(tabs.length > 0 && tabs[0].hasOwnProperty("url")){
-            let newUrl = "github.com"
+            let newUrl = "https://github.com/FongYoong/remove_scihub_lady"
             chrome.tabs.update(tabs.id, {url: newUrl});
         }
     });
